3413bebf70fab8274afba2ea82884b36449de1f0
Rupesh0109 <rupesh2486123@gmail.com>
2024-05-20 23:38:50 +0530

    Update sketch_may10c.ino

--- Project/Code/sketch_may10c.ino
+++ Project/Code/sketch_may10c.ino
@@ -1,57 +1,67 @@
 #define BLYNK_TEMPLATE_ID "TMPL3GCDace8O"
 #define BLYNK_TEMPLATE_NAME "gas"
-#include <WiFi.h>
-#include <BlynkSimpleEsp32.h>
+#include <Arduino.h>
+#include <BlynkSimpleEsp32.h> // Include Blynk library
 
-#define ANALOG_PIN 34 
-#define SERVO_PIN 18
-#define BUZZER_PIN 12 
+#define ANALOG_PIN 34
+#define SERVO_PIN 4
+#define BUZZER_PIN 12
 #define FAN_PIN 21
-#define LPG_THRESHOLD 20 
-#define BLYNK_TOKEN "8Vt_thWCzMfF1PxIxs0D1f5sAepocpj3" // Replace "YourAuthToken" with your Blynk authentication token
-#define WIFI_SSID "Rupesh12" // Replace "YourSSID" with your Wi-Fi SSID
-#define WIFI_PASSWORD "123456789" // Replace "YourPassword" with your Wi-Fi password
+#define LPG_THRESHOLD 20
 
-BlynkTimer timer;
+// Blynk app credentials (replace with your own)
+char auth[] = "8Vt_thWCzMfF1PxIxs0D1f5sAepocpj3";
+char ssid[] = "Rupesh12";
+char pass[] = "123456789";
 
-void sendLpgEvent() {
-  Blynk.virtualWrite(V0, "LPG Gas detected!");
+unsigned long previousMillis = 0;
+const long interval = 2000; // Interval to check LPG gas (milliseconds)
+
+BLYNK_WRITE(V1) // Define virtual pin for receiving commands
+{
+  int angle = param[0].asInt(); // Read angle value from app
+  moveServo(angle);
 }
 
 void setup() {
   Serial.begin(9600);
-  Blynk.begin(BLYNK_TOKEN, WIFI_SSID, WIFI_PASSWORD);
   pinMode(SERVO_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(FAN_PIN, OUTPUT);
 
-  timer.setInterval(1000L, []() {
-    int value = analogRead(ANALOG_PIN);
-    value = map(value, 0, 4095, 0, 100);
-    Blynk.virtualWrite(V1, value);
-    
-    if (value > LPG_THRESHOLD) {
-      Serial.println(" LPG Gas detected!");
-      Serial.print(value);
-      moveServo(0);
-      activateBuzzer();
-      activateFan();
-      sendLpgEvent();
-      delay(1000);
-    } else {
-      moveServo(180);
-      deactivateBuzzer();
-      deactivateFan();
-      Serial.println(" No LPG Gas detected!");
-      Serial.print(value);
-      delay(500);
-    }
-  });
+  Blynk.begin(auth, ssid, pass); // Initialize Blynk connection
 }
 
 void loop() {
-  Blynk.run();
-  timer.run();
+  Blynk.run(); // Run Blynk library
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    int value = analogRead(ANALOG_PIN);
+    value = map(value, 0, 4095, 0, 100);
+
+    if (value > LPG_THRESHOLD) {
+      Serial.println("LPG Gas detected!");
+      Serial.print("Value: ");
+      Serial.println(value);
+      Blynk.virtualWrite(V2, value); 
+      moveServo(0);
+      activateBuzzer();
+      activateFan();
+      delay(1000);
+    } else {
+      Serial.println("No LPG Gas detected!");
+      Serial.print("Value: ");
+      Serial.println(value);
+      Blynk.virtualWrite(V2, value); // Send LPG value to app
+      moveServo(180);
+      deactivateBuzzer();
+      deactivateFan();
+      delay(500);
+    }
+  }
 }
 
 void moveServo(int angle) {
@@ -63,11 +73,11 @@ void moveServo(int angle) {
 }
 
 void activateBuzzer() {
-  digitalWrite(BUZZER_PIN, HIGH); 
+  digitalWrite(BUZZER_PIN, HIGH);
 }
 
 void deactivateBuzzer() {
-  digitalWrite(BUZZER_PIN, LOW); 
+  digitalWrite(BUZZER_PIN, LOW);
 }
 
 void activateFan() {
@@ -75,6 +85,5 @@ void activateFan() {
 }
 
 void deactivateFan() {
-  digitalWrite(FAN_PIN, LOW); 
+  digitalWrite(FAN_PIN, LOW);
 }
-
